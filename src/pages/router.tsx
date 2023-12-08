ca61a250a8491174e44ab93c6fe1ac0aed2ef0ea
Cheleste1994 <wolverines@tut.by>
2023-12-08 11:15:07 +0300

    fix: changed name component page404, load suspense

--- src/pages/router.tsx
+++ src/pages/router.tsx
@@ -4,24 +4,26 @@ import { Routes, Route, Outlet } from 'react-router-dom';
 const MainPage = lazy(() => import('./MainPage/MainPage'));
 const SignInPage = lazy(() => import('./SignInPage/SignInPage'));
 const SignUpPage = lazy(() => import('./SignUpPage/SignUpPage'));
-const PageError = lazy(() => import('./404/404'));
+const Page404 = lazy(() => import('./404/404'));
 
 const Router = (): React.JSX.Element => (
   <Routes>
     <Route
       path=""
       element={
-        <Suspense fallback={<>Loading..</>}>
+        <>
           <>Header Component</>
-          <Outlet />
+          <Suspense fallback={<>Loading..</>}>
+            <Outlet />
+          </Suspense>
           <>Footer Component</>
-        </Suspense>
+        </>
       }
     >
       <Route path="/" element={<MainPage />} />
       <Route path="/signin" element={<SignInPage />} />
       <Route path="/signup" element={<SignUpPage />} />
-      <Route path="*" element={<PageError />} />
+      <Route path="*" element={<Page404 />} />
     </Route>
   </Routes>
 );

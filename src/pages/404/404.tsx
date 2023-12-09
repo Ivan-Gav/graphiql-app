fa2356538121566af3555dd84c745acf56496dd1
Cheleste1994 <wolverines@tut.by>
2023-12-09 11:56:49 +0300

    feat: add page 404

--- src/pages/404/404.tsx
+++ src/pages/404/404.tsx
@@ -1,3 +1,31 @@
+import { Typography, Button, Stack } from '@mui/material';
+import { NavLink } from 'react-router-dom';
+import UndoIcon from '@mui/icons-material/Undo';
+
 export default function Page404() {
-  return <> Page 404 </>;
+  return (
+    <main>
+      <Stack sx={{ width: '40%', margin: '0 auto', gap: 2 }}>
+        <Typography variant="h2" textAlign="center">
+          Page Not Found
+        </Typography>
+        <Typography variant="h4">
+          We could not find what you were looking for.
+        </Typography>
+        <Typography variant="h6">
+          Please contact the owner of the site that linked you to the original
+          URL and let them know their link is broken.
+        </Typography>
+        <Button
+          component={NavLink}
+          to="/"
+          variant="contained"
+          size="large"
+          endIcon={<UndoIcon />}
+        >
+          Home
+        </Button>
+      </Stack>
+    </main>
+  );
 }

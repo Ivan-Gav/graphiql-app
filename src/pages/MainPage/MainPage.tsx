63b8b9b96971719b927fe6c4959e2cc38d5ffff4
Ivan G <mr.zero@inbox.ru>
2023-12-11 23:06:55 +0300

    refactor: remove demo from main page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,15 @@
-import Demo from '../../components/ui/demo/Demo';
+// import Demo from '../../components/ui/demo/Demo';
+import { useLangContext } from '../../context/useLangContext';
+import { T } from '../../constants/text';
+import { Typography } from '@mui/material';
 
 export default function MainPage() {
-  return <Demo />;
+  const { lang } = useLangContext();
+
+  return (
+    <Typography variant="h1" mt={20}>
+      {T.HI[lang]}
+    </Typography>
+    // <Demo />
+  );
 }

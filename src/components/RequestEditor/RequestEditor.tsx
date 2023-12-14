e6c1f75a40d1f8f836673b01f72f11cb608c15ed
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-12-14 03:59:38 +0300

    feat: add RequestEditor textfield

--- src/components/RequestEditor/RequestEditor.tsx
+++ src/components/RequestEditor/RequestEditor.tsx
@@ -1,10 +1,38 @@
 import React from 'react';
-import { Paper } from '@mui/material';
+import { TextField } from '@mui/material';
+
+const defaultString = `{
+  characters(page: 2, filter: { name: "rick" }) {
+    info {
+      count
+    }
+    results {
+      name
+    }
+  }
+  location(id: 1) {
+    id
+  }
+  episodesByIds(ids: [1, 2]) {
+    id
+  }
+}
+`;
 
 export default function RequestEditor() {
+  const handleChange = () => {
+    console.log('handleChange');
+  };
   return (
     <>
-      <Paper variant="outlined">RequestEditor</Paper>
+      <TextField
+        variant="outlined"
+        multiline
+        rows={20}
+        defaultValue={defaultString}
+        onChange={handleChange}
+        fullWidth
+      />
     </>
   );
 }

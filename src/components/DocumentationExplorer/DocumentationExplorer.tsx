80917abe1e5e6f477e55c57cd9355a9b9ac990bc
Ivan G <mr.zero@inbox.ru>
2023-12-17 16:42:48 +0300

    feat: add temporary schema api for doc explorer

--- src/components/DocumentationExplorer/DocumentationExplorer.tsx
+++ src/components/DocumentationExplorer/DocumentationExplorer.tsx
@@ -1,9 +1,27 @@
 import { Paper } from '@mui/material';
+import { GraphQLSchema } from 'graphql';
+import { useEffect, useState } from 'react';
+import { getSchema } from 'src/api/apiSchema';
 
 export default function DocumentationExplorer() {
+  // temporary section for dev purposes -- start
+  const [schema, setSchema] = useState<GraphQLSchema | null>(null);
+
+  useEffect(() => {
+    async function fetchSchema() {
+      const s = await getSchema();
+      console.log(s);
+      setSchema(s instanceof GraphQLSchema ? s : null);
+    }
+    fetchSchema();
+  }, []);
+  // temporary section for dev purposes -- end
+
   return (
     <>
-      <Paper sx={{ p: 2 }}>DocumentationExplorer</Paper>
+      <Paper sx={{ p: 2 }}>
+        {schema ? 'We have a schema' : 'No schema found'}
+      </Paper>
     </>
   );
 }

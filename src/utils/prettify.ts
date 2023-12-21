a2c042d343185382fdeab068db3dc7949eb53ce8
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-12-21 05:17:27 +0300

    fix: change formatter

--- src/utils/prettify.ts
+++ src/utils/prettify.ts
@@ -1,5 +1,6 @@
 const addTabs = (str: string, count: number, space: string) => {
   let row = str.trim();
+
   for (let i = 0; i < count; i++) {
     if (row !== '') row = space + row;
   }
@@ -8,68 +9,29 @@ const addTabs = (str: string, count: number, space: string) => {
   return row;
 };
 
-const addSpaces = (el: string, inParentheses: boolean) => {
-  let newEl = el;
-  switch (el) {
-    case '}':
-      if (inParentheses) {
-        newEl = ' ' + el;
-      }
-      break;
-    case '{':
-      if (inParentheses) {
-        newEl = el + ' ';
-      } else {
-        newEl = ' ' + el;
-      }
-      break;
-    case '(':
-      break;
-    case ')':
-      break;
-    case ':':
-      newEl = el + ' ';
-      break;
-    case ',':
-      newEl = el + ' ';
-      break;
-    default:
-      break;
-  }
-  return newEl;
-};
-
 export const prettify = (str: string) => {
   const arr = [];
   let count = 0;
   let row = '';
   let inParentheses = false;
   const space = '  ';
+  let isTab = false;
+
+  str += '\n';
 
   for (let i = 0; i < str.length; i++) {
-    if (str[i] === '\n' || str[i] === ' ') continue;
-    const el = addSpaces(str[i], inParentheses);
+    const el = str[i];
 
-    if (el.includes('}')) {
-      if (!inParentheses) {
-        row = addTabs(row, count, space);
-        if (row !== '\n') arr.push(row);
-        row = '';
-
-        row += el;
-        count--;
-        arr.push(addTabs(row, count, space));
-        row = '';
-      } else {
-        row += el;
-      }
-    } else {
-      row += el;
-    }
-
-    if (el.includes('{') && !inParentheses) {
-      arr.push(addTabs(row, count, space));
-      count++;
+    row += el;
+    if (el.includes('}') && !inParentheses) {
+      count--;
+    } else if (el.includes('{') && !inParentheses) {
+      isTab = true;
+    } else if (el.includes('\n')) {
+      const finalRow = addTabs(row, count, space);
+      if (finalRow !== '' && finalRow !== '\n') arr.push(finalRow);
+      if (isTab) count++;
+      isTab = false;
       row = '';
     }
 

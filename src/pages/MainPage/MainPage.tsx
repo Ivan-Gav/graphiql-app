d39228da334a96658d4db749b81b26d6ff06fbe0
Ana-Ma-Web <90105049+Ana-Ma-Web@users.noreply.github.com>
2023-12-29 02:46:34 +0300

    fix: change textAlign

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,76 +1,83 @@
 // import Demo from '../../components/ui/demo/Demo';
-import { Grid, Link, Paper, Typography } from '@mui/material';
+import { Box, Grid, Link, Paper, Typography } from '@mui/material';
 import { useText } from 'src/hooks/useText';
 
 export default function MainPage() {
   const T = useText();
 
   return (
-    <>
-      <Grid container p={2} spacing={6}>
-        <Grid item xs={12}>
-          <Typography variant="h1" align="center">
-            {T.HI}
-          </Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
-              <Paper sx={{ padding: { xs: 2, md: 5 }, height: '100%' }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={4} md={12} lg={4} textAlign="center">
-                    <Link href="https://rs.school/react">
-                      <img
-                        src="https://www.svgrepo.com/show/424896/react-logo-programming-2.svg"
-                        alt="RSS"
-                        height={40}
-                        style={{ filter: 'invert(85%)' }}
-                      />
-                    </Link>
-                  </Grid>
-                  <Grid item xs={12} sm={8} md={12} lg={8}>
-                    <Typography variant="body1">{T.ABOUT_COURSE}</Typography>
-                  </Grid>
+    <Box p={2}>
+      <Grid item xs={12}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              textAlign: { xs: 'center', sm: 'left', md: 'center', lg: 'left' },
+            }}
+          >
+            <Paper sx={{ padding: { xs: 2, md: 5 }, height: '100%' }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={4} md={12} lg={4} textAlign="center">
+                  <Link href="https://rs.school/react">
+                    <img
+                      src="https://www.svgrepo.com/show/424896/react-logo-programming-2.svg"
+                      alt="RSS"
+                      height={40}
+                      style={{ filter: 'invert(85%)' }}
+                    />
+                  </Link>
                 </Grid>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={6}>
-              <Paper sx={{ padding: { xs: 2, md: 5 }, height: '100%' }}>
-                <Grid container spacing={2}>
-                  <Grid item xs={12} sm={4} md={12} lg={4} textAlign="center">
-                    <Link href="https://github.com/Ivan-Gav/graphiql-app">
-                      <img
-                        src="https://www.svgrepo.com/show/394181/graphql.svg"
-                        alt="RSS"
-                        height={40}
-                        style={{ filter: 'invert(85%)' }}
-                      />
-                    </Link>
-                  </Grid>
-                  <Grid item xs={12} sm={8} md={12} lg={8}>
-                    <Typography variant="body1">{T.ABOUT_PROJECT}</Typography>
-                  </Grid>
+                <Grid item xs={12} sm={8} md={12} lg={8}>
+                  <Typography variant="body1">{T.ABOUT_COURSE}</Typography>
                 </Grid>
-              </Paper>
-            </Grid>
-            <Grid item xs={12} md={12}>
-              <Paper sx={{ padding: { xs: 2, md: 5 } }}>
-                <Grid container spacing={5}>
-                  <Grid item xs={12} md={4}>
-                    <Typography variant="body1">{T.ABOUT_ANASTASIA}</Typography>
-                  </Grid>
-                  <Grid item xs={12} md={4}>
-                    <Typography variant="body1">{T.ABOUT_ANDREY}</Typography>
-                  </Grid>
-                  <Grid item xs={12} md={4}>
-                    <Typography variant="body1">{T.ABOUT_IVAN}</Typography>
-                  </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            sx={{
+              textAlign: { xs: 'center', sm: 'left', md: 'center', lg: 'left' },
+            }}
+          >
+            <Paper sx={{ padding: { xs: 2, md: 5 }, height: '100%' }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={4} md={12} lg={4} textAlign="center">
+                  <Link href="https://github.com/Ivan-Gav/graphiql-app">
+                    <img
+                      src="https://www.svgrepo.com/show/394181/graphql.svg"
+                      alt="RSS"
+                      height={40}
+                      style={{ filter: 'invert(85%)' }}
+                    />
+                  </Link>
                 </Grid>
-              </Paper>
-            </Grid>
+                <Grid item xs={12} sm={8} md={12} lg={8}>
+                  <Typography variant="body1">{T.ABOUT_PROJECT}</Typography>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={12} sx={{ textAlign: 'center' }}>
+            <Paper sx={{ padding: { xs: 2, md: 5 } }}>
+              <Grid container spacing={5}>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="body1">{T.ABOUT_ANASTASIA}</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="body1">{T.ABOUT_ANDREY}</Typography>
+                </Grid>
+                <Grid item xs={12} md={4}>
+                  <Typography variant="body1">{T.ABOUT_IVAN}</Typography>
+                </Grid>
+              </Grid>
+            </Paper>
           </Grid>
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 }

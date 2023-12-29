01eb166ab9122903e9149f81407309a88c41f9ab
Cheleste1994 <wolverines@tut.by>
2023-12-29 21:51:42 +0300

    feat: add tests docs

--- src/__tests__/mockSchema.ts
+++ src/__tests__/mockSchema.ts
@@ -4,9 +4,27 @@ export const schema = buildSchema(`
   type User {
     id: ID!
     firstName: String!
+    description: String
+    locationType: LocationType!
   }
- 
+
+  enum LocationType {
+    SPACESHIP
+    HOUSE
+    CAMPSITE
+    APARTMENT
+    ROOM
+  }
+
   type Query {
     user(id: ID!): User
+  }
+
+  type Mutation {
+    setMessage(message: String): String
+  }
+
+  type Subscription {
+    getUser: User
   }
 `);
